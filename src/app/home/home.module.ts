660d1c343cf5b8248d2bb6554fc103e6a93f42a8
edcan-dev <israel.da.queenie@gmail.com>
2023-09-10 15:29:46 -0600

    100923/fixed-unused-modules

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ProfileModule } from '../profile/profile.module';
 import { SharedModule } from '../shared/shared.module';
-import { MatCommonModule } from '@angular/material/core';
-
 @NgModule({
   imports: [
     ProfileModule,
